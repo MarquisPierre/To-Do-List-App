bbf0764f408744a1ae5a2991003269f0ea783cb7
Marquis <marquispierre27@gmail.com>
2022-06-15 23:51:06 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,39 @@
+const express = require("express")
+const bodyParser = require("body-parser")
+const app = express()
+const port = 3000
+let items = []
+
+console.log(items.length )
+app.set('view engine' , 'ejs')
+app.use(bodyParser.urlencoded({extended: true}))
+app.use("/public", express.static("public"))
+
+
+
+app.get("/" , (req, res)=>{
+    let today = new Date()
+    let options = {
+        weekday: 'long',
+        day: 'numeric',
+        month: 'long'
+    };
+
+    let day = today.toLocaleDateString("en-US", options)
+
+     res.render("list" , {kindOfDay: day , newListItems: items })
+
+
+})
+
+app.post("/" , (req, res)=>{
+    let item = req.body.newItem
+     items.push(item)
+    res.redirect("/")
+    
+})
+
+
+app.listen(port, ()=>{
+    console.log(`Server is up and running on port ${port}`)
+})
