07e05138d9973c0dca73dbaee47ac5d40e552232
Marquis <marquispierre27@gmail.com>
2022-06-22 01:29:21 -0400

    Created a module for date functions and created bew components with ejs

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require("express")
 const bodyParser = require("body-parser")
+const date = require(__dirname + "/date.js")
+
 const app = express()
 const port = 3000
-let items = []
+let items = ["study", "cook","code"]
+let workItems = []
 
-console.log(items.length )
 app.set('view engine' , 'ejs')
 app.use(bodyParser.urlencoded({extended: true}))
 app.use("/public", express.static("public"))
@@ -12,26 +14,45 @@ app.use("/public", express.static("public"))
 
 
 app.get("/" , (req, res)=>{
-    let today = new Date()
-    let options = {
-        weekday: 'long',
-        day: 'numeric',
-        month: 'long'
-    };
 
-    let day = today.toLocaleDateString("en-US", options)
+    let day = date.getDay()
 
-     res.render("list" , {kindOfDay: day , newListItems: items })
+     res.render("list" , {listTitle: day , newListItems: items })
 
 
 })
-
 app.post("/" , (req, res)=>{
+    console.log(req.body);
     let item = req.body.newItem
-     items.push(item)
-    res.redirect("/")
+
+    if (req.body.list === "Work") {
+        workItems.push(item)
+        res.redirect("/work")
+    }
+      else {
+        items.push(item)
+        res.redirect("/")
+        
+      }
+
     
+     
 })
+
+
+app.get("/work" , (req, res) =>{
+    res.render("list" , {listTitle: "Work List", newListItems: workItems})
+})
+app.post("/work" , (req, res) => {
+    
+    let item = req.body.newItem
+    workItems.push(item)
+    res.redirect("/work")
+})
+app.get('/about', (req, res) => {
+  res.render("about")
+})
+
 
 
 app.listen(port, ()=>{
