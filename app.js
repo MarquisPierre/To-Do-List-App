a129c2c96b01b6ef9391b295860c11549108b6da
Marquis Pierre <43345914+MarquisPierre@users.noreply.github.com>
2022-06-28 23:27:13 -0400

    Update app.js

--- app.js
+++ app.js
@@ -15,22 +15,22 @@ mongoose.connect('mongodb://localhost:27017/todolistDB')
 
 const itemsSchema = new mongoose.Schema({
   name: String
-})
+});
 
 
 const Item = mongoose.model("items", itemsSchema)
 
 const item1 = new Item({
   name: "Welcome to the TodoList!"
-})
+});
 
 const item2 = new Item({
   name: "Hit the + button to add a new item."
-})
+});
 
 const item3 = new Item({
   name: "<--Hit this to delete an item."
-})
+});
 
 
 const defaultItems = [item1, item2, item3]
